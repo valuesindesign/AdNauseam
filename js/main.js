d73676d7866052717a42a62c12e12bba3a05b0e3
Daniel Howe <daniel@rednoise.org>
2016-07-19 17:04:31 -0400

    added js for install-switching and tmp fix for #295

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
-	var original;
-    $("#logo.subpage").mouseover(function(){
-    	original= $(this).attr('src');
-        $(this).attr('src','img/adn_animated_croped.png');
-    }); 
-    $("#logo.subpage").mouseout(function(){
-        $(this).attr('src',original);
-    });
+$(document).ready(function () {
+
+  var original;
+
+	$("#logo.subpage").mouseover(function () {
+    original = $(this).attr('src');
+    $(this).attr('src', 'img/adn_animated_croped.png');
+  });
+
+  $("#logo.subpage").mouseout(function () {
+    $(this).attr('src', original);
+  });
+
+	// show the correct default install link
+	$("#install a").addClass('passive');
+	$("#install" + bowser.name).removeClass('passive');
+
+	// temp: pending #295
+	if (bowser.name === 'Opera')
+		$("#installChrome").css('right', '0px');
 });
